df16e18c2ec32edc0323ec910c41a768dc9dab65
Shannon Burchall <techreefbda@gmail.com>
2020-10-27 18:40:12 -0300

    add bowling model to api

---
+++ TruSportWebService/Models/BowlingGame.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace OnTrackWebService.Models
+{
+    public class BowlingGame
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public string ID { get; set; }
+        public string BowlingFixtureID { get; set; }
+        public string TeamID { get; set; }
+        public string BowlingRosterID { get; set; }
+        public int Score { get; set; }
+        public int Game { get; set; }
+
+        [ForeignKey("FixtureID")]
+        public Fixture Fixture { get; set; }
+
+        [ForeignKey("TeamID")]
+        public Team Team { get; set; }
+
+        [ForeignKey("BowlingRosterID")]
+        public BowlingRoster BowlingRoster { get; set; }
+
+    }
+}
